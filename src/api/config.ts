eaa0c1596837e7089d8489b09cc568541bf08425
Mauricio-mty <00000422@uca.edu.sv>
2025-11-03 08:29:54 -0600

    files

--- src/api/config.ts
+++ src/api/config.ts
@@ -1,3 +1,3 @@
-export const API_BASE_URL = import.meta.env.VITE_API_BASE_URL || "http://localhost:8000";
+export const API_BASE_URL = import.meta.env.VITE_API_BASE_URL || "http://192.168.1.155:8000";
 
 
