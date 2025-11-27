0e02a5703386cec3a84ca8e18bac349b4d539337
Mauricio-mty <00000422@uca.edu.sv>
2025-11-27 08:48:58 -0600

    sidebar add new route and fixed icon

--- src/components/Sidebar/Sidebar.tsx
+++ src/components/Sidebar/Sidebar.tsx
@@ -1,34 +1,75 @@
-import SidebarItem from "./SidebarItem";
+import { useState } from "react";
+import {SidebarItem} from "./SidebarItem";
+import { LogOut,ChevronLeft,TableProperties,ClipboardList,GitCompareArrows,FileText } from "lucide-react";
 import { useAuth } from "../../contexts/AuthContext";
-import { useNavigate } from "react-router-dom";
 
 const Sidebar = () => {
-  const { logout, user } = useAuth();
-  const navigate = useNavigate();
 
-  const handleLogout = () => {
-    logout();
-    navigate("/");
-  };
+  const [isCollapsed, setIsCollapsed] = useState(false);
+  const { logout } = useAuth();
+  
+ return (
+    <aside
+      className={`min-h-screen sticky top-0 bg-slate-800 text-white p-4 transition-all duration-300 ease-in-out flex flex-col  flex-shrink-0 ${
+        isCollapsed ? "w-20" : "w-64"
+      }`}
+    >
+      <div>
+        <div className="flex items-center justify-between mb-6">
+          <h2
+            className={`text-xl font-bold transition-opacity duration-300 ${
+              isCollapsed ? "opacity-0" : "opacity-100"
+            }`}
+          >
+            Panel
+          </h2>
+          <button
+            onClick={() => setIsCollapsed(!isCollapsed)}
+            className="p-1.5 rounded-lg bg-slate-700 hover:bg-slate-600"
+          >
+            <ChevronLeft
+              className={`transition-transform duration-300 ${
+                isCollapsed ? "rotate-180" : ""
+              }`}
+            />
+          </button>
+        </div>
 
-  return (
-    <aside className="h-screen w-64 bg-gray-800 text-white flex flex-col">
-      <div className="p-4 border-b border-gray-700">
-        <h2 className="text-2xl font-semibold">Mi App</h2>
+        <nav className="flex flex-col gap-2">
+          <SidebarItem
+            to="/general"
+            label="Tabla General"
+            isCollapsed={isCollapsed}
+            icon={<TableProperties />}
+          />
+          <SidebarItem
+            to="/resumen"
+            label="Tabla Resumida"
+            isCollapsed={isCollapsed}
+            icon={<ClipboardList />}
+          />
+          <SidebarItem
+            to="/comparacion"
+            label="Comparación"
+            isCollapsed={isCollapsed}
+            icon={<GitCompareArrows />}
+          />
+           <SidebarItem
+            to="/formato"
+            label="Formato"
+            isCollapsed={isCollapsed}
+            icon={<FileText />}
+          />
+        </nav>
       </div>
-      <nav className="flex-grow">
-        <SidebarItem to="/general" label="General" />
-        <SidebarItem to="/resumen" label="Resumen" />
-        <SidebarItem to="/comparacion" label="Comparacion" />
-        <SidebarItem to="/formato" label="Formato" />
-      </nav>
-      <div className="p-4 border-t border-gray-700">
-        <p className="text-sm">Bienvenido, {user?.username}</p>
+
+      <div>
         <button
-          onClick={handleLogout}
-          className="w-full mt-4 bg-red-600 hover:bg-red-700 text-white font-bold py-2 px-4 rounded"
+          onClick={logout}
+          className="w-full flex items-center gap-4 p-2 rounded-lg hover:bg-slate-700 border-t border-slate-700 pt-4 mt-2"
         >
-          Cerrar sesión
+          <LogOut />
+          {!isCollapsed && <span>Cerrar Sesión</span>}
         </button>
       </div>
     </aside>
